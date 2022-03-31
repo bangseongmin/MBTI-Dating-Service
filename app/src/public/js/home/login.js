a19cd2fe07d81fc64ae2f53d9154c682722b5444
bangseongmin <bangseongmin@naver.com>
2022-03-31 18:12:10 +0900

    DOM 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 'use strict';
 
-console.log('Hello2');
-console.log('Hello3');
+const id = document.querySelector('#id');
+const pw = document.querySelector('#pw');
+const loginBtn = document.querySelector('button');
+
+// 로그인 시
+loginBtn.addEventListener('click', login);
+
+
+function login() {
+    const req = {
+        id: id.value,
+        pw: pw.value
+    }
+
+    console.log(req);
+}
