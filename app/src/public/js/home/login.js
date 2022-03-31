eb0a5d30d96df80b7a6feaedc02f153fd12d6b52
bangseongmin <bangseongmin@naver.com>
2022-03-31 20:45:46 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,21 +7,29 @@ const loginBtn = document.querySelector('button');
 // 로그인 시
 loginBtn.addEventListener('click', login);
 
-
 function login() {
     const req = {
         id: id.value,
-        pw: pw.value
-    }
+        pw: pw.value,
+    };
 
-    console.log(req);
-    console.log(JSON.stringify(req));
+    // console.log(req);
+    // console.log(JSON.stringify(req));
     
     fetch("/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(req)
-    })
+        body: JSON.stringify(req),
+    }) 
+    .then((res) => res.json())     // then은 서버에서 응답한 데이터
+
+    /* res.json()의 반환값은 Promise다.
+       기본 res의 반환 값은 Response 스트림인데,
+       .json() 메소드를 통해 Response(응답) 스트림을 읽을 수 있다.
+       Response는 데이터가 모두 받아진 상태가 아니다.
+       .json으로 Resonse 스트림을 가져와 완료될때까지 읽는다.
+       다 읽은 body의 텍스트를 Promise 형태로 반환한다. */
+       .then(console.log);
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,45 @@
 "use strict";
 
-const home = (req, res)=>{
-    res.render("home/index");
+const output = {
+    home : (req, res)=>{
+        res.render("home/index");
+    },
+    
+    login : (req, res)=>{
+        res.render("home/login")
+    }
 };
 
-const login =  (req, res)=>{
-    res.render("home/login")
+/* 테스트 값 */
+const users = {
+    id: ["admin", "나개발", "김팀장"],
+    pw: ["1234", "12345", "123456"],
 };
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id;
+        const pw = req.body.pw;
+        
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            
+            if(users.pw[idx] === pw){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+    },
+};
+
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 }
