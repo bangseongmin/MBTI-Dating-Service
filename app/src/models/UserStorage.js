ddd983b5ed13bab72f22a26770af69d56f9009cc
bangseongmin <bangseongmin@naver.com>
2022-03-31 22:09:04 +0900

    User 모델 구현 및 UserStorage 수정

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -22,6 +22,18 @@ class UserStorage {
 
         return newUsers;
     }
+
+    static getUserInfo(id){
+        const users = this.#users;
+        const idx = users.id.indexOf(id);       // 해당 ID의 인덱스
+        const usersKeys = Object.keys(users);   // => [id, pw, name]
+        const userInfo = usersKeys.reduce((newUser, info)=>{
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
+    }
 }
 
 module.exports = UserStorage;
