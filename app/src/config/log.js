b16067cdebb25638dbbdbb30d37294c656f7223a
bangseongmin <bangseongmin@naver.com>
2022-04-04 16:12:17 +0900

    최종 최적화

--- app/src/config/log.js
+++
@@ -1,7 +0,0 @@
-// log variable
-const fs = require("fs");
-const appRoot = require("app-root-path");
-
-const accessLogStream = fs.createWriteStream(`${appRoot}/log/access.log`, { flags: 'a' });
-
-module.exports = accessLogStream;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -5,16 +5,17 @@ const logger = require('../../config/logger');
 
 const output = {
     home : (req, res)=>{
-        logger.info(`GET / 200 "홈 화면으로 이동"`);
+        logger.info(`GET / 304 "홈 화면으로 이동"`);
         res.render("home/index");
     },
     
     login : (req, res)=>{
-        logger.info(`GET /login 200 "로그인 화면으로 이동"`);
+        logger.info(`GET /login 304 "로그인 화면으로 이동"`);
         res.render("home/login")
     },
+
     register: (req, res) =>{
-        logger.info(`GET /register 200 "회원가입 화면으로 이동"`);
+        logger.info(`GET /register 304 "회원가입 화면으로 이동"`);
         res.render("home/register");
     }
 };
@@ -24,24 +25,30 @@ const process = {
     login: async(req, res) => {
         const user = new User(req.body);
         const response = await user.login();
+        
+        const url = {
+            method: "/POST",
+            path: "/login",
+            status: response.err ? 400 : 200,
+        };
 
-        if(response.err)
-            logger.error(`POST /login 200 "success: ${response.success}, ${response.err}"`);
-        else
-            logger.info(`POST /login 200 "success: ${response.success}, msg: ${response.msg}"`);
+        log(response, url);
 
-        return res.json(response);
+        return res.status(url.status).json(response);
     },
     register: async(req, res) =>{
         const user = new User(req.body);
         const response = await user.register();
 
-        if(response.err)
-            logger.error(`POST /register 200 "success: ${response.success}, ${response.err}"`);
-        else
-            logger.info(`POST /register 200 "success: ${response.success}, msg: ${response.msg}"`);
+        const url = {
+            method: "/POST",
+            path: "/register",
+            status: response.err ? 409 : 201,           // 서버측 에러(DB 등)가 발생할 경우의 반환할 상태코드는 500번대, 이미 등록되어 있는 것은 클라이언트 측의 잘못이므로 409
+        };
 
-        return res.json(response);
+        log(response, url);
+
+        return res.status(url.status).json(response);
     }
 };
 
@@ -49,4 +56,11 @@ const process = {
 module.exports = {
     output,
     process,
+}
+
+const log = (response, url) => {
+    if(response.err)
+        logger.error(`${url.method} ${url.path} ${url.status} Response : ${response.success} ${response.err}`);
+    else
+        logger.info(`${url.method} ${url.path} ${url.status} Response : ${response.success} ${response.msg || ""}`);
 }
