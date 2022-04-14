fc88d2c492830410cd5f133d9fa4cbe2d7cfae0b
bangseongmin <bangseongmin@naver.com>
2022-04-15 00:56:41 +0900

    카카오 공유 완료

--- app/src/public/js/home/share.js
+++ app/src/public/js/home/share.js
@@ -1,3 +1,36 @@
+const url = 'http://localhost:3000/';
+
+function setShare(){
+    var resultImg = document.querySelector('#resultImg');
+    var resultAlt = resultImg.firstElementChild.alt;
+    const shareTitle = '십이간지 연애유형 결과';
+    const shareDes = infoList[resultAlt].name;
+    const shareImage = url + 'image' + resultAlt;
+    const shareURL = url + 'result-' + resultAlt;
+
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+        title: shareTitle,
+        description: shareDes,
+        imageUrl: shareImage,
+        link: {
+            mobileWebUrl: shareURL,
+            androidExecutionParams: shareURL,
+        },
+        },
+        buttons: [
+        {
+            title: '결과확인하기',
+            link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL,
+            },
+        }
+        ]
+    });
+}
+
 function kakaoShare(){
     Kakao.Link.sendDefault({
         objectType: 'feed',
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -19,18 +19,31 @@ router.post('/findUsername', ctrl.process.findUsername);
 router.post('/findPassword', ctrl.process.findPassword);
 router.post('/updatePassword', ctrl.process.updatePassword);
 
-router.get('/result-0', ctrl.show.result0);
-router.get('/result-1', ctrl.show.result1);
-router.get('/result-2', ctrl.show.result2);
-router.get('/result-3', ctrl.show.result3);
-router.get('/result-4', ctrl.show.result4);
-router.get('/result-5', ctrl.show.result5);
-router.get('/result-6', ctrl.show.result6);
-router.get('/result-7', ctrl.show.result7);
-router.get('/result-8', ctrl.show.result8);
-router.get('/result-9', ctrl.show.result9);
-router.get('/result-10', ctrl.show.result10);
-router.get('/result-11', ctrl.show.result11);
+router.get('/result-0', ctrl.result.result0);
+router.get('/result-1', ctrl.result.result1);
+router.get('/result-2', ctrl.result.result2);
+router.get('/result-3', ctrl.result.result3);
+router.get('/result-4', ctrl.result.result4);
+router.get('/result-5', ctrl.result.result5);
+router.get('/result-6', ctrl.result.result6);
+router.get('/result-7', ctrl.result.result7);
+router.get('/result-8', ctrl.result.result8);
+router.get('/result-9', ctrl.result.result9);
+router.get('/result-10', ctrl.result.result10);
+router.get('/result-11', ctrl.result.result11);
+
+router.get('/image0', ctrl.image.image0);
+router.get('/image1', ctrl.image.image1);
+router.get('/image2', ctrl.image.image2);
+router.get('/image3', ctrl.image.image3);
+router.get('/image4', ctrl.image.image4);
+router.get('/image5', ctrl.image.image5);
+router.get('/image6', ctrl.image.image6);
+router.get('/image7', ctrl.image.image7);
+router.get('/image8', ctrl.image.image8);
+router.get('/image9', ctrl.image.image9);
+router.get('/image10', ctrl.image.image10);
+router.get('/image11', ctrl.image.image11);
 
 
 module.exports = router;
